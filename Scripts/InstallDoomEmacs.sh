00a00ae30d41737f9f13acaa527a6aa83c58e1ee
Penek Suksuda <admin@peneksuksuda.com>
2023-11-09 22:28:50 +0700

    Added Doom Emacs Installation Script

---
+++ Scripts/InstallDoomEmacs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git clone --depth 1 https://github.com/doomemacs/doomemacs $HOME/.config/emacs
+$HOME/.config/emacs/bin/doom install
+
--- Scripts/InstallNerdFonts.sh
+++ Scripts/InstallNerdFonts.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts $HOME/NERDFONTS
-cd $HOME/NERDFONTS
-./install.sh
+$HOME/NERDFONTS/install.sh
 
